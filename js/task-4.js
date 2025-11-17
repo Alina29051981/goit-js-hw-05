222035675d9dc0c1b9bbbf9634f512bf4f9e9377
Alina Kuchma <ostaliva@ukr.net>
2025-11-17 17:09:54 +0200

    added fix version

--- js/task-4.js
+++ js/task-4.js
@@ -2,7 +2,7 @@ const getTotalBalanceByGender = (users, gender) =>
   users
     .filter((user) => user.gender === gender)
     .map((user) => user.balance)
-    .reduce((total, balance) => total + balance);
+    .reduce((total, balance) => total + balance, 0);
 
 const clients = [
   {
